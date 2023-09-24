6c04f3ec673be078fc7dafdf582becf0e230f8af
maverick9453 <amankumar9453@gmail.com>
2023-09-24 17:56:44 +0530

    first commit

---
+++ string_indexing.js
@@ -0,0 +1,8 @@
+let firstName = "harshit";
+
+// h   a   r   s    h     i       t
+// 0   1   2   3    4     5       6
+
+// we can access any index by the helip of square bracket [ ]
+
+console.log(firstName[0]);
